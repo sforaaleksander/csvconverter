370bfc8712702cc1f13a1b067b391c8ef10f0bc3
sforaaleksander <sforaaleksander@gmail.com>
2020-10-28 16:44:58 +0100

    implement args validation

--- src/main/java/com/codecool/csvconverter/App.java
+++ src/main/java/com/codecool/csvconverter/App.java
@@ -8,7 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        
-        System.out.println( "Hello World!" );
+        if (args.length < 1) {
+            System.out.println("Please provide file name to convert");
+            return;
+        }
+        Converter converter = new Converter();
+        if (args.length == 1) {
+            converter.convert(args[0], SupportedFormat.TABLE);
+            return;
+        }
+        converter.convert(args[1], SupportedFormat.valueOf(args[0].toUpperCase()));
     }
 }
