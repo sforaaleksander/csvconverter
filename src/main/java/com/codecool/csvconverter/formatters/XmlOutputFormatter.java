3b8cf25c0b742930ab561d05c53072b1a2857d6e
sforaaleksander <sforaaleksander@gmail.com>
2020-10-29 13:20:43 +0100

    implement xml formatting

--- src/main/java/com/codecool/csvconverter/formatters/XmlOutputFormatter.java
+++ src/main/java/com/codecool/csvconverter/formatters/XmlOutputFormatter.java
@@ -1,11 +1,23 @@
 package com.codecool.csvconverter.formatters;
 
-import java.io.*;
-
 public class XmlOutputFormatter implements OutputFormatter {
 
     @Override
     public void appendElementsToStringBuilder(StringBuilder sb, String[] elements) {
-
+        sb.append("<");
+        for (int i=0;i<elements.length;i++) {
+            if (elements[i].equals("")){
+                continue;
+            }
+            sb.append(i+1);
+            sb.append("=");
+            sb.append('"');
+            sb.append(elements[i]);
+            sb.append('"');
+            if (i != elements.length-1) {
+                sb.append(" ");
+            }
+        }
+        sb.append("/>");
     }
 }
