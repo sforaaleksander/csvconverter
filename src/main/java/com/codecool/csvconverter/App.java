1d56fe57ed49ee5865d123eec8deae25fc6269a4
sforaaleksander <sforaaleksander@gmail.com>
2020-10-29 19:18:26 +0100

    provisional working xml solution

--- src/main/java/com/codecool/csvconverter/App.java
+++ src/main/java/com/codecool/csvconverter/App.java
@@ -3,6 +3,9 @@ package com.codecool.csvconverter;
 import com.codecool.csvconverter.converter.Converter;
 import com.codecool.csvconverter.filereader.FileReader;
 import com.codecool.csvconverter.formatters.SupportedFormat;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 import java.io.File;
 
@@ -14,9 +17,17 @@ public class App
             System.out.println("Please provide file name to convert");
             return;
         }
-        File file = new File(args[0]);
-        FileReader fileReader = new FileReader(file);
-        Converter converter = new Converter(fileReader);
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/config.xml");
+
+//        File file = applicationContext.getBean(File.class, args[0]);
+
+        File file = (File) applicationContext.getBean("file");
+
+        System.out.println(file.getName());
+
+        FileReader fileReader = (FileReader) applicationContext.getBean("fileReader", file);
+        Converter converter = (Converter) applicationContext.getBean("converter", fileReader);
         if (args.length == 1) {
             converter.convert(SupportedFormat.TABLE);
             return;
